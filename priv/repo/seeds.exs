2bcf95425b93e42a7f2b1b95e9036a97fd166bfd
Patrick <patrick@pop-os.localdomain>
2023-11-14 10:35:57 -0800

    added faker as a dependency to dev and test to create fake author info.
    added seeding for fake authors to seeds.exs.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,18 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+import Faker.Person.En
+import Faker.Lorem
+alias BookTracker.Repo
+alias BookTracker.Authors
+
+1..100
+|> Enum.map(fn _ -> 
+  %{
+    first_name: first_name(),
+    last_name: last_name(),
+    bio_notes: paragraph() 
+  }
+end) 
+|> Enum.each(&Authors.create_author/1)
+
