ccdc2d0a3ce1d776efc029e20674cb0af6280a24
Patrick <patrick@pop-os.localdomain>
2023-11-15 09:31:32 -0800

    changeset for authors and genres

--- lib/book_tracker/books/book.ex
+++ lib/book_tracker/books/book.ex
@@ -2,6 +2,7 @@ defmodule BookTracker.Books.Book do
   use Ecto.Schema
   import Ecto.Changeset
   alias BookTracker.Authors.Author
+  alias BookTracker.Genres.Genre
 
   schema "books" do
     field :title, :string
@@ -20,6 +21,22 @@ defmodule BookTracker.Books.Book do
     book
     |> cast(attrs, [:title, :page_count, :summary, :isbn10, :isbn13])
     |> validate_required([:title, :page_count, :summary, :isbn10, :isbn13])
+  end
+
+  @doc """
+  It is possible for this function to raise at run time if the last two arguments 
+  are not lists of author structs and genre structs respectively - it is up to the caller to provide this list!
+
+  Will create a changeset for a new book with the associated authors and genres.
+  """
+  def changeset_with_authors_and_genres(book, attrs, authors, genres) do
+    if all_author_structs?(authors) and all_genre_structs?(genres) do
+      changeset(book, attrs)
+      |> put_assoc(:genres, genres)
+      |> put_assoc(:authors, authors)
+    else
+      raise(ArgumentError, "the last two arguments must be a list of author structs and genre structs respectively.")
+    end
   end
 
   @doc """
@@ -45,4 +62,13 @@ defmodule BookTracker.Books.Book do
   end
 
   defp all_author_structs?(_authors), do: false
+
+  defp all_genre_structs?(genres) when is_list(genres) do
+    Enum.reduce(genres, true, fn
+      %Genre{} = _genre, acc -> acc
+      _, _ -> false
+    end)
+  end
+
+  defp all_genre_structs?(_authors), do: false
 end
