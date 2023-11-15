32324756ae2db73a2ffcf3e546d7a3efad763b28
Patrick <patrick@pop-os.localdomain>
2023-11-15 09:32:57 -0800

    added genres form field and handlers

--- lib/book_tracker_web/live/new_book_live.ex
+++ lib/book_tracker_web/live/new_book_live.ex
@@ -1,8 +1,7 @@
 defmodule BookTrackerWeb.NewBookLive do
   use BookTrackerWeb, :live_view
-  alias BookTracker.Books
   alias BookTracker.Books.Book
-  alias BookTracker.Authors
+  alias BookTracker.{Authors, Books, Genres}
   alias BookTrackerWeb.LiveComponents.MatchAndSelect
 
   def mount(_, _, socket) do
@@ -10,6 +9,7 @@ defmodule BookTrackerWeb.NewBookLive do
     |> assign(:book_form, to_form(Books.change_book(%Book{})))
     |> assign(:form_reset, false)
     |> assign(:selected_authors, [])
+    |> assign(:selected_genres, [])
     |> then(&{:ok, &1})
   end
 
@@ -22,12 +22,24 @@ defmodule BookTrackerWeb.NewBookLive do
       <.input type="number" field={@book_form[:page_count]} label="Page Count" />
       <.input type="text" field={@book_form[:isbn10]} label="ISBN-10" />
       <.input type="text" field={@book_form[:isbn13]} label="ISBN-13" />
+      <.input type="text" field={@book_form[:title]} label="Title" />
+      <.live_component
+        module={MatchAndSelect}
+        id="genre-select"
+        reset={@form_reset}
+        match_function={&Genres.get_genre_by_name/2}
+        input_label="Genres"
+        input_identifier="genres"
+        match_label="Matching Genres"
+        selected_label="Added to Book"
+      />
       <.live_component
         module={MatchAndSelect}
         id="author-select"
         reset={@form_reset}
         match_function={&Authors.get_author_by_name/2}
         input_label="Authors"
+        input_identifier="authors"
         match_label="Matching Authors"
         selected_label="Added to Book"
       />
@@ -38,8 +50,9 @@ defmodule BookTrackerWeb.NewBookLive do
 
   def handle_event("book-submitted", %{"book" => params}, socket) do
     IO.inspect(socket.assigns.selected_authors, label: "selected authors")
+    IO.inspect(socket.assigns.selected_genres, label: "selected genres")
 
-    case Books.create_book(params) do
+    case Books.create_book(params, socket.assigns.selected_authors, socket.assigns.selected_genres) do
       {:ok, _} ->
         socket
         |> put_flash(:info, "book added")
@@ -54,8 +67,12 @@ defmodule BookTrackerWeb.NewBookLive do
     end
   end
 
-  def handle_info({:selected_update, selected_list}, socket) do
+  def handle_info({:selected_update, "authors",selected_list}, socket) do
     {:noreply, assign(socket, :selected_authors, selected_list)}
+  end
+
+  def handle_info({:selected_update, "genres",selected_list}, socket) do
+    {:noreply, assign(socket, :selected_genres, selected_list)}
   end
 
   defp assign_new_book_form(socket) do
