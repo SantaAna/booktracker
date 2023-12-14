6745232c60530996b70537f20ce3a93b6ec56935
Patrick <patrick@pop-os.localdomain>
2023-12-13 21:40:54 -0800

    Updated params to use shishkebob format throughout to simplify parameter passing from the search form.
    Added logic for extracting the author first and last name from the
    author name string provided by the user.
    
    Added default values for books per page and author name to prevent
    errors when reloading a page with books per page unset.

--- lib/book_tracker_web/live/books_list_live.ex
+++ lib/book_tracker_web/live/books_list_live.ex
@@ -2,44 +2,52 @@ defmodule BookTrackerWeb.BooksListLive do
   use BookTrackerWeb, :live_view
   alias BookTracker.Books
 
+  @default_params %{
+    "page-size" => "5",
+    "page" => "1",
+    "author-name" => ""
+  }
+
   def mount(_, _, socket) do
     {:ok, socket}
   end
 
-  def handle_params(%{"page_size" =>  page_size, "page" => page}, _uri, socket) do
+  def handle_params(params, _uri, socket) do
+    params = Map.merge(@default_params, params)
+
+    [first_name, last_name] = extract_first_and_last(params["author-name"])
+
+    {maximum_pages, books} =
+      Books.search(
+        current_page: String.to_integer(params["page"]),
+        page_size: String.to_integer(params["page-size"]),
+        author_first_name: first_name,
+        author_last_name: last_name
+      )
+
     socket
-    |> assign(:books, Books.get_books_on_page(String.to_integer(page), String.to_integer(page_size), [:authors, :genres]))
-    |> assign(:page_size, String.to_integer(page_size))
-    |> assign(:page, String.to_integer(page))
-    |> assign(:max_page, Books.maximum_page_count(String.to_integer(page_size)))
+    |> assign(:books, books)
+    |> assign(:author_name, params["author-name"])
+    |> assign(:page_size, String.to_integer(params["page-size"]))
+    |> assign(:page, String.to_integer(params["page"]))
+    |> assign(:max_page, maximum_pages)
     |> then(&{:noreply, &1})
   end
-
-  def handle_params(%{"page_size" =>  page_size}, _uri, socket) do
-    socket
-    |> assign(:books, Books.get_books_on_page(1, String.to_integer(page_size), [:authors, :genres]))
-    |> assign(:page_size, String.to_integer(page_size))
-    |> assign(:page, 1)
-    |> assign(:max_page, Books.maximum_page_count(String.to_integer(page_size)))
-    |> then(&{:noreply, &1})
-  end
-
-
-  def handle_params(_params, _uri, socket) do
-    socket
-    |> assign(:books, Books.get_books_on_page(1, 5, [:authors, :genres]))
-    |> assign(:page_size, 5)
-    |> assign(:page, 1)
-    |> assign(:max_page, Books.maximum_page_count(5))
-    |> then(&{:noreply, &1})
-  end
-
 
   def render(assigns) do
     ~H"""
     <form phx-submit="search-params-updated">
-      <label for="books-per-page"> Books Per Page </label>
-      <input type="number" name="books-per-page" id="books-per-page" placeholder={@page_size}/>
+      <label for="page-size"> Books Per Page </label>
+      <input
+        type="number"
+        name="page-size"
+        id="page-size"
+        placeholder={@page_size}
+        value={@page_size}
+      />
+      <label for="author-name">Author Name</label>
+      <input type="text" name="author-name" id="author-name" value={@author_name} />
+      <.button>Submit</.button>
     </form>
     <table class="w-full">
       <thead class="border-b-2 border-gray-200 bg-gray-50">
@@ -54,7 +62,10 @@ defmodule BookTrackerWeb.BooksListLive do
       </thead>
       <tr :for={book <- @books} class="border-b border-gray-300 even:bg-gray-100">
         <td class="p-3 text-sm tracking-wide">
-          <.link navigate={~p"/books/#{book.id}"} class="underline underline-offset-2 hover:text-sky-700">
+          <.link
+            navigate={~p"/books/#{book.id}"}
+            class="underline underline-offset-2 hover:text-sky-700"
+          >
             <%= book.title %>
           </.link>
         </td>
@@ -64,26 +75,39 @@ defmodule BookTrackerWeb.BooksListLive do
       </tr>
     </table>
     <div class="flex flex-row justify-center pt-3 divide-x-4 divide-white ">
-      <div :if={@page > 1} class="rounded-l-lg text-left p-2 bg-gray-100"> 
-        <.link
-          patch={~p"/books?#{[page: (@page - 1), page_size: @page_size]}"}
-        >
-        Prev
+      <div :if={@page > 1} class="rounded-l-lg text-left p-2 bg-gray-100">
+        <.link patch={
+          ~p"/books?#{%{"page" => @page - 1, "page-size" => @page_size, "author-name" => @author_name}}"
+        }>
+          Prev
         </.link>
       </div>
-      <div :if={@page < @max_page} class="rounded-r-lg p-2 text-right bg-gray-100"> 
-        <.link
-          patch={~p"/books?#{[page: (@page + 1), page_size: @page_size]}"}
-        >
-        Next
+      <div :if={@page < @max_page} class="rounded-r-lg p-2 text-right bg-gray-100">
+        <.link patch={
+          ~p"/books?#{%{"page" => @page + 1, "page-size" => @page_size, "author-name" => @author_name}}"
+        }>
+          Next
         </.link>
       </div>
     </div>
     """
   end
 
-  def handle_event("search-params-updated",%{"books-per-page" => books_per_page} , socket) do
-    {:noreply, push_patch(socket, to: ~p"/books?#{[page_size: books_per_page]}")}
+  def handle_event("search-params-updated", params, socket) do
+    {:noreply, push_patch(socket, to: ~p"/books?#{params}")}
+  end
+
+  defp extract_first_and_last(author_name) do
+    case String.split(author_name, " ", parts: 2) do
+      [first_name, last_name] ->
+        [first_name, last_name]
+
+      [first_name] ->
+        [first_name, nil]
+
+      _ ->
+        [nil, nil]
+    end
   end
 
   defp authors_to_names(authors) do
