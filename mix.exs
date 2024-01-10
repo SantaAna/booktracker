c39601874ab84c4820017ba823f4e6361aef0ff5
Patrick <patrick@pop-os.localdomain>
2024-01-09 17:53:17 -0800

    added earmark

--- mix.exs
+++ mix.exs
@@ -51,7 +51,8 @@ defmodule BookTracker.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:plug_cowboy, "~> 2.5"},
-      {:faker, "~> 0.17", only: [:test, :dev]}
+      {:faker, "~> 0.17", only: [:test, :dev]},
+      {:earmark, "~> 1.4"}
     ]
   end
 
