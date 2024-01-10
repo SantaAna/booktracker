5481905970a6ff81c879e5e905d286fb55df7213
Patrick <patrick@pop-os.localdomain>
2024-01-09 20:17:33 -0800

    updated author pages
    updated author pages to properly dispaly author information

--- lib/book_tracker_web/controllers/author_controller.ex
+++ lib/book_tracker_web/controllers/author_controller.ex
@@ -1,12 +1,13 @@
 defmodule BookTrackerWeb.AuthorController do
-  use BookTrackerWeb, :controller  
+  use BookTrackerWeb, :controller
   alias BookTracker.Authors
-  
+
   def show(conn, %{"author_id" => author_id}) do
-    case Authors.get_author(String.to_integer(author_id), [books: :genres]) do
-      nil -> 
-        render(conn, :notfound, author_id: author_id)
-      author -> 
+    case Authors.get_author(String.to_integer(author_id), books: :genres) do
+      nil ->
+        render(conn, :not_found, author_id: author_id)
+
+      author ->
         render(conn, :show, author: author)
     end
   end
--- lib/book_tracker_web/controllers/author_html.ex
+++ lib/book_tracker_web/controllers/author_html.ex
@@ -1,5 +1,29 @@
 defmodule BookTrackerWeb.AuthorHTML do
-  use BookTrackerWeb, :html    
-  
-  embed_templates "author_html/*"
+  use BookTrackerWeb, :html
+
+  def show(assigns) do
+    ~H"""
+    <.author_full_name author={@author}/>
+    <h2 class="font-semibold mb-3"> Biography </h2>
+    <div>
+      <%= raw(@author.bio_notes) %>
+    </div>
+    """
+  end
+
+  def not_found(assigns) do
+    ~H"""
+      <h1 class="text-lg font-semibold"> Author With ID <%= @author_id %> Not Found </h1>
+    """
+  end
+
+  attr :author, :map, required: true
+
+  def author_full_name(assigns) do
+    ~H"""
+    <h1 class="text-lg font-semibold mb-5">
+      <%= String.capitalize(@author.first_name) %> <%= String.capitalize(@author.last_name) %>
+    </h1>
+    """
+  end
 end
